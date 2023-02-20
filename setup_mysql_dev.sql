f299a5bdf0b2d8b2c86d5b49a0fb8b3d60d91f0a
Nathaniel81 <nathanield356@gmail.com>
2023-02-20 14:37:07 +0300

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,6 +6,8 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges to hbnb_test user
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Grants privilages to the user on performance_schema
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to ensure changes take effect
