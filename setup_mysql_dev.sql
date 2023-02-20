2a6780780c24d18496ea6868ad46a698a8278222
Nathaniel81 <nathanield356@gmail.com>
2023-02-20 03:34:09 -0800

    MySQL setup test

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Create hbnb_test_db if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create hbnb_test user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to hbnb_test user
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to ensure changes take effect
+FLUSH PRIVILEGES;
