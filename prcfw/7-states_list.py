d12fa148cd5b49f6703b3e2f855a719709366c52
Nathaniel81 <nathanield356@gmail.com>
2023-02-26 10:48:10 +0300

    Q7-dump

---
+++ prcfw/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine, Table, MetaData
+
+engine = create_engine("mysql+mysqldb://root:''@localhost/hbnb_dev_db")
+
+# Create a metadata object
+metadata = MetaData()
+
+# Reflect the structure of the database
+metadata.reflect(bind=engine)
+
+# Get the table object for mytable
+mytable = Table('cities', metadata, autoload=True, autoload_with=engine)
+
+# Create a database session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Query the mytable table
+result = session.query(mytable).all()
+
+# Print the results
+for row in result:
+    print(row)
+
+# Close the session
+session.close()
+
